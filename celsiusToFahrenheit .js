2b36ac9416d0167df9e0b3781287cff24a7b53a9
Conor Kennedy <kennedyconor93@gmail.com>
2017-07-29 23:07:08 -0500

    Create celsiusToFahrenheit .js

---
+++ celsiusToFahrenheit .js
@@ -0,0 +1,8 @@
+//Conor Kennedy
+//7/29/17
+
+//super basic celsius to fahrenheit converter in JS
+
+const celcius = prompt("Enter temp in celcius:");
+farheheit = celcius * 9/5 + 32;
+alert(`Farheheit = ${farheheit} `);
