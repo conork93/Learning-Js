c38bb293c92f9f3353347de871617935f120fb68
Conor Kennedy <kennedyconor93@gmail.com>
2017-08-08 11:33:52 -0500

    eloquentJS ch3p1

---
+++ min.js
@@ -0,0 +1,14 @@
+//Conor Kennedy
+//8/8/2017
+
+//Write a function min that takes two arguments and returns their minimum.
+
+function min(x, y){
+  if (x < y)
+    return x
+  else
+    return y
+}
+
+console.log(min(0, 10))
+console.log(min(0,-10))
